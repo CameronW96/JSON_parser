fc46f57bb661955c9b7e576eea746778dec8d4af
Cameron White <mynameiswade@gmail.com>
2021-08-09 13:53:59 -0400

    finished dot notation functions, return value functions, and read file function

--- JSON_parser/main.cpp
+++ JSON_parser/main.cpp
@@ -1,16 +1,31 @@
 #include "jsontr.h"
 #include <iostream>
+#include <assert.h>
 
-
+using namespace cjw;
 
 int main()
 {
 		std::string primitive_test = "{ int : 1, double : 1.332, bool : false, string : \"Hello World!\" }";
-		cjw::JSON_List list1 = cjw::JSON_List::parse(primitive_test);
+		JSON_List list1 = cjw::JSON_List::parse(primitive_test);
 
 		std::string array_test = "{ array : [1, 2, \"word\"] }";
-		cjw::JSON_List list2 = cjw::JSON_List::parse(array_test);
+		JSON_List list2 = cjw::JSON_List::parse(array_test);
 
 		std::string object_test = "{ object : { nestedKey: \"nestedValue\", anotherArray : [1, true, 1.443] } }";
-		cjw::JSON_List list3 = cjw::JSON_List::parse(object_test);
+		JSON_List list3 = cjw::JSON_List::parse(object_test);
+
+		int integer_return_test = JSON_List::r_int(list2.d_n("array").a_n(1));
+		assert(integer_return_test == 2);
+
+		double double_return_test = JSON_List::r_double(list1.d_n("double"));
+		assert(double_return_test == 1.332);
+
+		bool boolean_return_test = JSON_List::r_bool(list1.d_n("bool"));
+		assert(boolean_return_test == false);
+
+		std::string string_return_test = JSON_List::r_string(list2.d_n("array").a_n(2));
+		assert(string_return_test == std::string("word"));
+
+		std::string file_test = JSON_List::read_file("example.json");
 }
